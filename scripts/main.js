c430648adf7de9498ed396a06b27e7a7cbc71478
realpho3nix <55889540+realpho3nix@users.noreply.github.com>
2021-05-08 23:44:15 +0300

    Added new section and fixed some bug in header

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
 console.log('Script connected successfuly!')
 
 document.querySelector('.header__button').addEventListener('click', () => {
-    console.log('Clicked')
+    console.log('Clicked header button!')
+})
+
+document.querySelector('.providing__button').addEventListener('click', () => {
+    console.log('Clicked providing button!')
 })
