3ce5efe7d2256457a2350c840dd0e950e648bf9e
realpho3nix <55889540+realpho3nix@users.noreply.github.com>
2021-05-09 23:23:34 +0300

    Added new section, removed useless scripts

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-console.log('Script connected successfuly!')
-
-document.querySelector('.header__button').addEventListener('click', () => {
-    console.log('Clicked header button!')
-})
-
-document.querySelector('.providing__button').addEventListener('click', () => {
-    console.log('Clicked providing button!')
-})
---
+++ scripts/tabImageGenerator.js
@@ -0,0 +1,10 @@
+// export function tabImageGenerator(imagesArr, imgHTMLArr) {
+//     for(let i = 0; i < imgHTMLArr.length; i++) {
+//         if (imagesArr[i] === undefined) {
+//             imgHTMLArr[i].style.display = "none";
+//             break;
+//         }
+//         imgHTMLArr[i].style.display = "";
+//         imgHTMLArr[i].src = imagesArr[i];
+//     }
+// }
